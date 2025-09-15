9d5274a1a9ca5115aa652584e3670f0729476b8b
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-09-15 10:57:09 +0545

    Some changes

--- server.js
+++ server.js
@@ -1,51 +1,31 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
-
-// DB connection
+// Build DB string
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log('✅ Successfully connected to MongoDB'))
-//   .catch((err) => {
-//     console.error('❌ DB connection error:', err);
-//     process.exit(1);
-//   });
-
-let isConnected = false;
-
-async function connectToMongoDB() {
+async function startServer() {
   try {
-    await mongoose.connect(process.env.DATABASE, {
+    await mongoose.connect(DB, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    isConnected = true;
     console.log('✅ Successfully connected to MongoDB');
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+      console.log(`🚀 Server running on port ${port}...`);
+    });
   } catch (error) {
-    console.error('❌ DB connection error:', err);
+    console.error('❌ DB connection error:', error);
+    process.exit(1);
   }
 }
 
-// Start server
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`🚀 Server running on port ${port}...`);
-// });
-
-app.use((req, res, next) => {
-  if (!isConnected) {
-    connectToMongoDB();
-  }
-  next();
-});
+startServer();
